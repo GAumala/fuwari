fcbb5b901424b502c52e484996deb29260e0b017
Gabriel Aumala <gaumala@protonmail.com>
2026-01-28 18:16:11 -0500

    use aumala.dev domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gaumala.github.io/",
-  base: "/fuwari",
+  site: "https://aumala.dev",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind({
